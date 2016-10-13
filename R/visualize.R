45cc2ec7412b30730120fdf3d4acd08541326aa1
ekstroem <github@ekstroem.com>
2016-10-13 12:55:06 +0200

    Fixed exports

--- R/visualize.R
+++ R/visualize.R
@@ -1,35 +1,35 @@
 #'@title Produce marginal distribution plots
 #'
-#'@description Generic shell function that calls a plotting function in order to produce a marginal 
+#'@description Generic shell function that calls a plotting function in order to produce a marginal
 #'distribution plot for a variable, depending on the class of the variable.
 #'
 #'@param v The variable (vector) which is to be plotted.
-#'@param vnam The name of the variable. This name might be printed on the plots, depending on the 
+#'@param vnam The name of the variable. This name might be printed on the plots, depending on the
 #'choice of plotting function. If not supplied, it will default to the name of \code{v}.
-#'@param allVisuals The name (as a character string) of the actual plotting function to be used, no 
-#'matter the class of \code{v}. See \code{details} for more details about the structure of such plotting 
-#'functions. Note that this option is overwritten if a non-null class specific 
+#'@param allVisuals The name (as a character string) of the actual plotting function to be used, no
+#'matter the class of \code{v}. See \code{details} for more details about the structure of such plotting
+#'functions. Note that this option is overwritten if a non-null class specific
 #'plotting function is supplied in \code{...} (see \code{details}).
 #'@param doEval If TRUE, \code{visualize} returns a plot (IS THIS THE CORRECT WAY TO SAY IT?). Otherwise,
 #'visualize returns a character string containing R-code for producing a plot.
-#'@param ... ALLOW FOR ARGUMENTS TO BE PASSED ON TO E.G. STANDARDVISUAL AS WELL? If the plotting 
+#'@param ... ALLOW FOR ARGUMENTS TO BE PASSED ON TO E.G. STANDARDVISUAL AS WELL? If the plotting
 #'function to be used should differ by variable type, this can be specified by using additional arguments
-#'on the form \code{characterVisual = "standardVisual"}, ..., \code{logicalVisual = "standardVisual"} for 
-#'each of the 6 data classes \code{character}, \code{factor}, \code{labelled}, \code{numeric}, 
+#'on the form \code{characterVisual = "standardVisual"}, ..., \code{logicalVisual = "standardVisual"} for
+#'each of the 6 data classes \code{character}, \code{factor}, \code{labelled}, \code{numeric},
 #'\code{integer} and \code{logical}.
 #'
-#'@details The function supplied in \code{allVisuals} should take a variable, \code{v}, and its name, 
-#'\code{vnam}, and an evaluation indicator, \code{doEval}, and return a character string containing 
-#'(standalone) code for producing a plot if \code{doEval = FALSE} and a plot otherwise. 
-#'See e.g. \code{\link{standardVisual}} for an example of such a plotting function and the example 
+#'@details The function supplied in \code{allVisuals} should take a variable, \code{v}, and its name,
+#'\code{vnam}, and an evaluation indicator, \code{doEval}, and return a character string containing
+#'(standalone) code for producing a plot if \code{doEval = FALSE} and a plot otherwise.
+#'See e.g. \code{\link{standardVisual}} for an example of such a plotting function and the example
 #'below.
 #'
-#'@examples 
+#'@examples
 #'  #Standard use: Return standalone code for plotting a function:
 #'    visualize(c(1:10), "Variable 1", doEval = FALSE)
-#'  
-#'  
-#'  #Define a new visualization function and call it using visualize either 
+#'
+#'
+#'  #Define a new visualization function and call it using visualize either
 #'  #using allVisual or a class specific argument:
 #'    pieVisual <- function(v, vnam, doEval) {
 #'      thisCall <- call("pie", x=table(v), main=vnam)
@@ -37,21 +37,21 @@
 #'        return(eval(thisCall))
 #'      } else return(deparse(thisCall))
 #'    }
-#'    
+#'
 #'  \dontrun{
 #'    #use allVisual:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", allVisuals = "pieVisual")
-#'    
+#'
 #'    #use characterVisual:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", characterVisual = "pieVisual")
-#'    
+#'
 #'    #this will use standardVisual:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", numericVisual = "pieVisual")
 #'  }
-#'    
+#'
 #'    #return code for a pie chart
-#'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", allVisuals = "pieVisual", doEval=F)
-#'  
+#'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", allVisuals = "pieVisual", doEval=FALSE)
+#'
 #'  \dontrun{
 #'  #Produce multiple plots easily, depending on data type
 #'    data(testData)
@@ -63,7 +63,7 @@
 #'
 #'@seealso \code{\link{standardVisual}}, \code{\link{basicVisual}}
 #'@export
-visualize <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+visualize <- function(v, vnam = NULL, allVisuals = "standardVisual",
                       doEval = FALSE, ...) UseMethod("visualize")
 
 
@@ -73,20 +73,23 @@ visualize <- function(v, vnam = NULL, allVisuals = "standardVisual",
 
 
 #Methods for each variable type
-visualize.character <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+#' @export
+visualize.character <- function(v, vnam = NULL, allVisuals = "standardVisual",
                                 characterVisual=NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
   useVisual <- ifelse(is.null(characterVisual), allVisuals, characterVisual)
   eval(call(useVisual, v, vnam, doEval))
 }
 
-visualize.factor <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+#' @export
+visualize.factor <- function(v, vnam = NULL, allVisuals = "standardVisual",
                              factorVisual = NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
   useVisual <- ifelse(is.null(factorVisual), allVisuals, factorVisual)
   eval(call(useVisual, v, vnam, doEval))
 }
 
+#' @export
 visualize.labelled <- function(v, vnam = NULL, allVisuals = "standardVisual",
                                labelledVisual = NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
@@ -94,20 +97,23 @@ visualize.labelled <- function(v, vnam = NULL, allVisuals = "standardVisual",
   eval(call(useVisual, v, vnam, doEval))
 }
 
-visualize.numeric <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+#' @export
+visualize.numeric <- function(v, vnam = NULL, allVisuals = "standardVisual",
                               numericVisual = NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
   useVisual <- ifelse(is.null(numericVisual), allVisuals, numericVisual)
   eval(call(useVisual, v, vnam, doEval=doEval))
 }
 
-visualize.integer <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+#' @export
+visualize.integer <- function(v, vnam = NULL, allVisuals = "standardVisual",
                               integerVisual = NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
   useVisual <- ifelse(is.null(integerVisual), allVisuals, integerVisual)
   eval(call(useVisual, v, vnam, doEval))
 }
 
+#' @export
 visualize.logical <- function(v, vnam = NULL, allVisuals = "standardVisual",
                               logicalVisual = NULL, doEval = FALSE, ...) {
   if (is.null(vnam)) vnam <- deparse(substitute(v))
