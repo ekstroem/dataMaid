583bc05658c8c052038cdd877c513d5793a84a53
annepetersen1 <ahpe@sund.ku.dk>
2016-10-13 12:40:10 +0200

    First draft of summarize function documentation done

---
+++ R/countMissing.R
@@ -0,0 +1,22 @@
+#' @title Summary function for missing values
+#' 
+#' @description A summary type function to be called from \code{\link{summarize}}, which counts the
+#' number of missing (NA) values in a variable.
+#' 
+#' @param v A variable (vector).
+#' 
+#' @return A list with $feature: "No. missing obs." and 
+#' $result: [the number and percentage missing observations]. 
+#' 
+#' @seealso \code{\link{summarize}}
+#' 
+#' @examples
+#' countMissing(c(1:100, rep(NA, 10)))
+#' 
+#' @export
+countMissing <- function(v) {
+  noMissing <- sum(is.na(v))
+  percentMissing <- round(100*noMissing/length(v),2)
+  list(feature="No. missing obs." ,
+       result=paste(noMissing, " (", percentMissing," %)", sep=""))
+}
---
+++ R/uniqueValues.R
@@ -0,0 +1,47 @@
+#' @title Summary function for unique values
+#' 
+#' @description A summary type function to be called from \code{\link{summarize}}, which counts the
+#' number of unique (excluding NA) values in a variable.
+#' 
+#' @param v A variable (vector).
+#' 
+#' @return A list with $feature: "No. unique values" and 
+#' $result: [the number of unique values]. 
+#' 
+#' @seealso \code{\link{summarize}}
+#' 
+#' @examples
+#' uniqueValues(c(1:3, rep(NA, 10), Inf, NaN))
+#' 
+#' @export
+uniqueValues <- function(v) UseMethod("uniqueValues")
+
+
+
+
+##########################################Not exported below#########################################
+
+
+#methods for each variable type
+
+# #' @importFrom stats na.omit
+uniqueValuesCFLBI <- function(v) {
+  noUnique <- length(unique(na.omit(v)))
+  list(feature="No. unique values", result=noUnique)
+}
+
+uniqueValuesN <- function(v) {
+  out <- uniqueValuesCFLBI(v)
+  #check for NaNs
+  if (any(is.nan(v))) out$result <- out$result + 1
+  out
+}
+
+
+#assign methods to generic uniqueValues function
+uniqueValues.character <- function(v) uniqueValuesCFLBI(v)
+uniqueValues.factor <- function(v) uniqueValuesCFLBI(v)
+uniqueValues.labelled <- function(v) uniqueValuesCFLBI(v) #?PROBLEM?
+uniqueValues.numeric <- function(v) uniqueValuesN(v)
+uniqueValues.integer <- function(v) uniqueValuesCFLBI(v)
+uniqueValues.logical <- function(v) uniqueValuesCFLBI(v)
