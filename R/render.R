9485c6eec5029104640edbec8a7b3ee70bd0f4c6
ekstroem <github@ekstroem.com>
2016-12-09 17:19:42 +0100

    Made render internal

--- R/render.R
+++ R/render.R
@@ -1,6 +1,6 @@
 
-
-#make pdf/html from a .Rmd file
-#' @export
+# make pdf/html from a .Rmd file
+# CE: made this an internal function  # ' @export
+# This function just calls the corresponding function from rmarkdown
 render <- function(file, quiet) rmarkdown::render(file, quiet=quiet)
 
