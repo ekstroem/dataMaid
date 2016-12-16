a254a19d2563395643a77e7ea3fae4806c0bb2ab
ekstroem <github@ekstroem.com>
2016-12-16 19:23:49 +0100

    summaryResult.R - added documentation (should be checked)

--- R/summaryResult.R
+++ R/summaryResult.R
@@ -1,3 +1,21 @@
+#' Create object of class summaryResult
+#'
+#' Covert a list resulting from the summaries performed in a
+#' \code{\link{summaryFunction}} into a \code{summaryResult} object, thereby
+#' supplying it with a \code{print()} method.
+#'
+#' @param ls A list with entries \code{$problem} (logical indicating whether
+#' a problem was found), \code{$message} (a character string containing a
+#' message describing the problem) and \code{$problemValues} (the values
+#' in the checked variables that were marked as problematic). Note that
+#' \code{$message} and \code{$problemValues} can be left empty (i.e.
+#' \code{""} and \code{NULL}, respectively), if they are not relevant.  XXX
+#'
+#' @return A S3 object of class \code{summaryResult}, identical to the inputted
+#' list, \code{ls}, except for its class attribute.
+#'
+#' @seealso \code{\link{summaryFunction}}
+#'
 #' @export
 summaryResult <- function(ls) {
   if (length(setdiff(names(ls), c("feature", "result", "value"))) != 0) {
