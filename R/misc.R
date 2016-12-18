d33d747d3c60e689aef895a07a9cfdb1c4ff889f
annepetersen1 <ahpe@sund.ku.dk>
2016-12-18 16:24:11 +0100

    Made new files for these functions

---
+++ R/misc.R
@@ -0,0 +1,13 @@
+#Miscellaneous methods that do not belong in any specfic
+#other file. Only methods are allowed in this file, other
+#functions, however minor, should have their own files.
+
+#Print overview of all functions of a certain  type, used in 
+#allVisualFunctions(), allSummaryFunctions(), 
+#allCheckFunctions().
+#' @importFrom pander pander
+#' @export
+print.functionSummary <- function(x, ...) {
+  x$classes <- sapply(x$classes, function(x) paste(x, collapse=", "))
+  pander::pander(data.frame(x, row.names = NULL), justify="left")
+}
---
+++ R/unpackLabelled.R
@@ -0,0 +1,9 @@
+#Maybe delete? Deal when labelled stuff is implemented.
+#this function is used in identifyMissing.R and
+#identifyWhitespace.R
+#' @export
+unpackLabelled <- function(v) {
+  c(as.character(v), attributes(v)$labels)
+}
+
+
