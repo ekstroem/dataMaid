ba09d80983fa247a9ec53de38c626b66845bf64f
ekstroem <github@ekstroem.com>
2016-12-20 10:06:22 +0100

    docu

--- R/makeXFunction.R
+++ R/makeXFunction.R
@@ -1,24 +1,24 @@
 
 #Make a function of type X (among visualFunction, summaryFunction and
-#checkFunction). If classes is null, and fName is a S3 generic, 
+#checkFunction). If classes is null, and fName is a S3 generic,
 #the function will make a look-up for all methods available in the
 #global enviroment and fill out the slot accordingly.
-#Note: This does not work for functions defined wihtin cleanR. As 
+#Note: This does not work for functions defined wihtin cleanR. As
 #of now, they must have their classes specified explicitly!
-#'@importFrom utils methods
+#' @importFrom utils methods
 makeXFunction <- function(fName, description, classes, X) {
   f <- get(fName)
-  
+
   if (is.null(description)) description <- fName
   if (is.null(classes)) {
     theseMethods <- as.character(methods(fName)) #methods() needs the name in order
     #to work inside the function
-    
+
     #if (length(theseMethods) == 0) {
     #  callEnv <- parent.env(as.environment(-1L))
     #  theseMethods <- as.character(utils::.S3methods(fName, envir = callEnv))
     #}
-    
+
     if (length(theseMethods) > 0) {
       classes <- sub(paste(fName, ".", sep=""),
                      "", theseMethods)
