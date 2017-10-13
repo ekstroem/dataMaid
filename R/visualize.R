a770e7946bb347d2195c42d127d9e8b4db22f387
annepetersen1 <ahpe@sund.ku.dk>
2017-10-13 15:12:19 +0200

    Updated argument structure in visualize() to match that of makeDataReport() (using setVisuals()).

--- R/visualize.R
+++ R/visualize.R
@@ -9,11 +9,11 @@
 #' @param vnam The name of the variable. This name might be printed on the plots, depending on the
 #' choice of plotting function. If not supplied, it will default to the name of \code{v}.
 #'
-#' @param allVisuals The name (as a character string) of the actual plotting function to be used, no
-#' matter the class of the variable. Note that if \code{v} is a single variable, this option is 
-#' overwritten if a non-null class specific plotting function is supplied in \code{...} 
-#' (see \emph{details} below). However, if \code{v} is a dataset, the function chosen in 
-#' \code{allVisuals} will decide what plots are made.
+#' @param visuals A list of visual functions to use on each supported variable type. We recommend
+#' using \code{\link{setVisuals}} for creating this list and refer to the documentation
+#' of this function for more details. This function allows for choosing variable-type dependent 
+#' visuals. However, if \code{visualize()} is called on a full dataset, all visualizations
+#' must be of the same type and therefore, the \code{all} argument of \code{setVisuals} is used.
 #'
 #' @param doEval A logical. If \code{TRUE} (the default), \code{visualize} has the side effect of
 #' producing a plot (or multiple plots, if \code{v} is a data.frame). Otherwise,
@@ -23,12 +23,10 @@
 #' @param ... Additional arguments used for class-specific choices of visual functions 
 #' (see \emph{details}).
 #'
-#' @details Visual functions can be  supplied using their names (in character strings) in 
-#' the class-specific arguments, e.g.\code{characterVisual = "standardVisual"}, ..., 
-#' \code{logicalVisual = "standardVisual"} for each of the 7 data classes \code{character}, 
-#' \code{Date}, \code{factor}, \code{labelled}, \code{numeric}, \code{integer} and \code{logical}. 
-#' 
-#' Note that an overview of all available \code{visualFunction}s can be obtained by calling
+#' @details Visual functions can be  supplied using their names (in character strings) using
+#' \code{setVisuals}. Note that only a single visual function is allowed for each variable class.
+#' The default visual settings can be inspected by calling \code{setVisuals()}.  
+#' An overview of all available \code{visualFunction}s can be obtained by calling
 #' \code{\link{allVisualFunctions}}. 
 #'
 #' A user defined visual function can be supplied using its function name. Details on how 
@@ -49,19 +47,23 @@
 #'    mosaicVisual <- visualFunction(mosaicVisual, 
 #'                                   description = "Mosaicplots from graphics",
 #'                                   classes = allClasses())
+#'   
+#'   #Inspect all options for visualFunctions:
+#'   allVisualFunctions()
+#'
 #'
 #'  \dontrun{
-#'    #use allVisual:
+#'    #set mosaicVisual for all variable types:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", 
-#'        allVisuals = "mosaicVisual")
+#'        visuals = setVisuals(all = "mosaicVisual"))
 #'
-#'    #use characterVisual:
+#'    #set mosaicVisual only for character variables:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", 
-#'       characterVisual = "mosaicVisual")
+#'       visuals = setVisuals(character = "mosaicVisual"))
 #'
-#'    #this will use standardVisual:
+#'    #this will use standardVisual, as our variable is not numeric:
 #'    visualize(c("1", "1", "1", "2", "2", "a"), "My variable", 
-#'        numericVisual = "mosaicVisual")
+#'        visuals = setVisuals(numeric = "mosaicVisual"))
 #'  }
 #'
 #'    #return code for a mosaic plot
@@ -76,15 +78,17 @@
 #'    
 #'  #When using visualize on a dataset, datatype specific arguments have no
 #'  #influence:
-#'    visualize(testData2, factorVisual = "basicVisual", characterVisual = "basicVisual")
+#'    visualize(testData2, setVisuals(character = "basicVisual", 
+#'        factor = "basicVisual"))
 #'    
-#'  #But the "allVisuals" argument still works:
-#'    visualize(testData2, allVisuals = "basicVisual")
+#'  #But we can still use the "all" argument in setVisuals:
+#'    visualize(testData2, visuals =  setVisuals(all = "basicVisual"))
 #'  }
 #'
-#'@seealso \code{\link{standardVisual}}, \code{\link{basicVisual}}
+#'@seealso \code{\link{setVisuals}}, \code{\link{allVisualFunctions}},
+#'\code{\link{standardVisual}}, \code{\link{basicVisual}}
 #'@export
-visualize <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize <- function(v, vnam = NULL, visuals = setVisuals(),
                       doEval = TRUE, ...) UseMethod("visualize")
 
 
@@ -95,74 +99,75 @@ visualize <- function(v, vnam = NULL, allVisuals = "standardVisual",
 #Methods for each variable type
 
 #' @export
-visualize.character <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.character <- function(v, vnam = NULL, visuals = setVisuals(),
                                 doEval = TRUE, characterVisual = NULL, ...) {
+  if (is.null(characterVisual)) characterVisual <- visuals$character 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(characterVisual), allVisuals, characterVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
+  eval(call(characterVisual, v = v, vnam = vnam, doEval = doEval))
 }
 
 #' @export
-visualize.factor <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.factor <- function(v, vnam = NULL, visuals = setVisuals(),
                              doEval = TRUE, factorVisual = NULL, ...) {
+  if (is.null(factorVisual)) factorVisual <- visuals$factor 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(factorVisual), allVisuals, factorVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
-}
+  eval(call(factorVisual, v = v, vnam = vnam, doEval = doEval))}
 
 #' @export
-visualize.labelled <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.labelled <- function(v, vnam = NULL, visuals = setVisuals(),
                                doEval = TRUE, labelledVisual = NULL, ...) {
+  if (is.null(labelledVisual)) labelledVisual <- visuals$labelled 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(labelledVisual), allVisuals, labelledVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
-}
+  eval(call(labelledVisual, v = v, vnam = vnam, doEval = doEval))}
 
 
 #' @export
-visualize.numeric <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.numeric <- function(v, vnam = NULL, visuals = setVisuals(),
                               doEval = TRUE, numericVisual = NULL, ...) {
+  if (is.null(numericVisual)) numericVisual <- visuals$numeric 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(numericVisual), allVisuals, numericVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
-}
+  eval(call(numericVisual, v = v, vnam = vnam, doEval = doEval))}
 
 #' @export
-visualize.integer <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.integer <- function(v, vnam = NULL, visuals = setVisuals(),
                               doEval = TRUE, integerVisual = NULL, ...) {
+  if (is.null(integerVisual)) integerVisual <- visuals$integer 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(integerVisual), allVisuals, integerVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
-}
+  eval(call(integerVisual, v = v, vnam = vnam, doEval = doEval))}
 
 #' @export
-visualize.logical <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.logical <- function(v, vnam = NULL, visuals = setVisuals(),
                               doEval = FALSE, logicalVisual = NULL, ...) {
+  if (is.null(logicalVisual)) logicalVisual <- visuals$logical 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(logicalVisual), allVisuals, logicalVisual)
-  eval(call(useVisual, v, vnam, doEval))
-}
+  eval(call(logicalVisual, v = v, vnam = vnam, doEval = doEval))}
 
 #' @export
-visualize.Date <- function(v, vnam = NULL, allVisuals = "standardVisual",
+visualize.Date <- function(v, vnam = NULL, visuals = setVisuals(),
                            doEval = TRUE, DateVisual = NULL, ...) {
+  if (is.null(DateVisual)) DateVisual <- visuals$Date 
   if (is.null(vnam)) vnam <- deparse(substitute(v))
-  useVisual <- ifelse(is.null(DateVisual), allVisuals, DateVisual)
-  eval(call(useVisual, v = v, vnam = vnam, doEval = doEval))
-}
+  eval(call(DateVisual, v = v, vnam = vnam, doEval = doEval))}
 
 
 #' @importFrom gridExtra marrangeGrob
 #' @export 
-visualize.data.frame <- function(v, vnam = NULL, allVisuals = "standardVisual", 
+visualize.data.frame <- function(v, vnam = NULL, visuals = setVisuals(), 
                                  doEval = TRUE, ...) {
   
-  #note: all plots have to be of the same type, hence only the allVisuals 
-  #argument is passed on to the variable level visualizations
-  plots <- lapply(names(v), function(x) {visualize(v[[x]], x, allVisuals = allVisuals,
+  #note: all plots have to be of the same type, hence only the first 
+  #choice of visual is passed on to the variable level visualizations
+  useVisuals <- setVisuals(all = visuals[[1]])
+  if(length(unique(visuals)) > 1) warning(paste("When visualizing a dataset,",
+                                                "all variable types must use the",
+                                                "same visualization, however, more",
+                                                "than one visualization was supplied.",
+                                                "Only the first was used."))
+  
+  plots <- lapply(names(v), function(x) {visualize(v[[x]], x, visuals = useVisuals,
                   doEval = doEval)})
   if (doEval) {
-    if (allVisuals == "standardVisual") {
+    if (useVisuals[[1]] == "standardVisual") {
       out <- marrangeGrob(plots, nrow=3, ncol=2)
       return(out)
     } 
