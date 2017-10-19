fe4118752820efb6910c7168c83d3316433c4842
annepetersen1 <ahpe@sund.ku.dk>
2017-10-19 12:56:41 +0200

    Fixed newline issue in @importFrom

--- R/standardVisual.R
+++ R/standardVisual.R
@@ -28,11 +28,7 @@
 #' }
 #' @seealso \code{\link{visualize}}, \code{\link{basicVisual}}
 #'
-#' @importFrom ggplot2 qplot geom_bar geom_rect ylab xlab aes_string ggplot aes
-<<<<<<< HEAD
-#' theme element_blank geom_text 
-=======
->>>>>>> 5cdd0679a87176f4f795f0dd82650f695b04f529
+#' @importFrom ggplot2 qplot geom_bar geom_rect ylab xlab aes_string ggplot aes theme element_blank geom_text 
 #' @importFrom stats na.omit
 #' @export
 standardVisual <- function(v, vnam, doEval = TRUE) UseMethod("standardVisual")
@@ -156,9 +152,6 @@ ggEmptyPlot <- function(v, vnam) {
           title = element_blank()) 
   p
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 5cdd0679a87176f4f795f0dd82650f695b04f529
 
 
